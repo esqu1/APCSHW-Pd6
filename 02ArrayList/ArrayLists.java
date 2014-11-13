88f66c73e339136e7c9fccd070d827e1450fed7b
esqu1 <brandbest1@gmail.com>
2014-11-12 19:59:38 -0500

    ArrayLists added

---
+++ 02ArrayList/ArrayLists.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class ArrayLists{
+
+    public static void collapseDuplicates(ArrayList<Integer> L){
+	for(int i = 0; i < L.size(); i++){
+	    while(true){
+		if(i < L.size() - 1 && L.get(i) == L.get(i+1)){
+		    L.remove(i+1);
+		}else{
+		    break;
+		}
+	    }
+	}
+    }
+
+
+    public static void main(String[] args){
+	ArrayList<Integer> L = new ArrayList<Integer>();
+	int[] A = {1,9,3,3,4,2,2,2,7,8,8,8,8,9};
+	for(int i = 0; i < A.length; i++){
+	    L.add(A[i]);
+	}
+	collapseDuplicates(L);
+	System.out.println(L);
+    }
+
+	
+
+
+}
