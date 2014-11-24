41d946af13f57f6e0d72e7bde46ef4672a6bf59e
esqu1 <brandbest1@gmail.com>
2014-11-24 13:19:12 -0500

    moved load to Driver

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -1,10 +1,23 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Driver{
 
     public static void main(String[] args){
+	Scanner s;
 	WordGrid p = new WordGrid(7,7);
-	p.load("words.txt");
-	p.randomAdd();
+	File F = new File("words.txt");
+	try{
+	    s = new Scanner(F);
+	}catch(FileNotFoundException e){
+	    s = new Scanner(System.in);
+	}
+	ArrayList<String> str = new ArrayList<String>();
+	while(s.hasNext()){
+	    str.add(s.next());
+	}
+	
+       	p.randomAdd();
 	System.out.println(p);
     }
 
