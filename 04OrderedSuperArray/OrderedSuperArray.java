d6acaffd7294c2972b5c7407e74a08da90bdd673
esqu1 <brandbest1@gmail.com>
2014-12-02 12:00:29 -0500

    add modified

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -7,8 +7,12 @@ public class OrderedSuperArray extends SuperArray{
     public void add(String element){
 	int index = size() - 1;
 	while(true){
-	    if(element.compareTo(get(index)) < 0){
-		add(index,element);
+	    if(index <= -1){
+		super.add(0,element);
+		break;
+	    }else if(element.compareTo(get(index)) < 0){
+		super.add(index,element);
+		break;
 	    }else{
 		index--;
 	    }
