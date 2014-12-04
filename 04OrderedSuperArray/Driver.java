d74fb38cac7d5f375325d98e0828c77b0a484261
esqu1 <brandbest1@gmail.com>
2014-12-04 12:24:19 -0500

    added badinsertion

--- 04OrderedSuperArray/Driver.java
+++ 04OrderedSuperArray/Driver.java
@@ -1,23 +1,27 @@
+import java.util.*;
 public class Driver{
 
+    public void badInsertionSort(SuperArray L){
+        OrderedSuperArray c = new OrderedSuperArray();
+        while( L.size() > 0){ 
+            c.add(L.remove(0));
+        }
+        while(c.size() > 0){
+            L.add(c.remove(0));
+        }
+    }
+
     public static void main(String[] args){
-	System.out.println("idea".compareTo("hi"));
-	OrderedSuperArray L = new OrderedSuperArray();
-	L.add("hi");
-	System.out.println(L);
-	System.out.println(L.size());
-	L.add("idea");
-	System.out.println(L);
-	L.add("apple");
-	System.out.println(L);
-	SuperArray M = new SuperArray();
-	M.add("you");
-	M.add("hi");
-	M.add("nyeh");
-	M.add("apple");
-	M.add("Hey");
+	SuperArray M = new SuperArray(2000);
+	Random r = new Random();
+	for(int i = 0; i < 2000; i++){
+	    int m = r.nextInt(26) + 97;
+	    M.add("" + (char)m);
+	}
+	
 	M.insertionSort();
 	System.out.println(M);
+
     }
 
 }
--- 04OrderedSuperArray/SuperArray.java
+++ 04OrderedSuperArray/SuperArray.java
@@ -111,4 +111,5 @@ public class SuperArray{
 	    set(j,temp);
 	}
     }
+
 }
