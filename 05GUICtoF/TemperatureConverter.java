7117cd31542d412de6be8ac4a71ef394c6ff1687
esqu1 <brandbest1@gmail.com>
2014-12-10 17:15:59 -0500

    actionlistener

--- 05GUICtoF/TemperatureConverter.java
+++ 05GUICtoF/TemperatureConverter.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class TemperatureConverter extends JFrame{
+public class TemperatureConverter extends JFrame implements ActionListener{
 
     private Container pane,texty,buttons;
-    private JButton CF;
-    private JTextField text;
+    private JButton CF,FC;
+    private JTextField text, output;
     private JLabel l;
 
     public TemperatureConverter(){
@@ -18,8 +18,15 @@ public class TemperatureConverter extends JFrame{
 	pane.setLayout(new FlowLayout());
 
 	CF = new JButton("Convert to Fahrenheit!");
+	CF.addActionListener(this);
+	CF.setActionCommand("CtoF");
+	FC = new JButton("Convert to Celsius!");
+	FC.addActionListener(this);
+	FC.setActionCommand("FtoC");
 	l = new JLabel("Enter temperature:",null,JLabel.LEFT);
 	text = new JTextField(12);
+	output = new JTextField(12);
+	output.setEditable(false);
 
 	buttons = new Container();
 	buttons.setLayout(new FlowLayout());
@@ -27,13 +34,28 @@ public class TemperatureConverter extends JFrame{
 	texty.setLayout(new FlowLayout());
 
 	buttons.add(CF);
+	buttons.add(FC);
 	texty.add(l);
 	texty.add(text);
+	texty.add(output);
 
 	pane.add(texty);
 	pane.add(buttons);
 	
     }
+
+    public void actionPerformed(ActionEvent e){
+	if(text.getText().equals("")){
+	    System.out.println("Please enter a number.");
+	}else if(e.getActionCommand().equals("CtoF")){
+	    int k = Double.parseDouble(text.getText());
+	    output.setText("" + ((double)(9 * k) / 5 + 32));
+	}else if(e.getActionCommand().equals("FtoC")){
+	    int k = Double.parseDouble(text.getText());
+	    output.setText("" + (double)(5 * (k - 32)) / 9);
+	}
+    }
+    
     public static void main(String[] args){
 	TemperatureConverter T = new TemperatureConverter();
 	T.setVisible(true);
