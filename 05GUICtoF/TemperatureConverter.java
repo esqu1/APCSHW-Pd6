43cc20a28b465a7a78ad11c0e8e19e4c69f74b53
esqu1 <brandbest1@gmail.com>
2014-12-10 12:22:58 -0500

    GUI folder

---
+++ 05GUICtoF/TemperatureConverter.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class TemperatureConverter extends JFrame implements ActionListener{
+
+    private Container pane,texty,buttons;
+    private JButton CF;
+    private JTextField text;
+    
+}
